35e7365ecef731c27183df6d651056463923f66f
xiaopei0107 <724152220@qq.com>
2018-11-08 14:40:06 +0800

    hello

--- src/main/java/com/zxp/controller/HelloController.java
+++ src/main/java/com/zxp/controller/HelloController.java
@@ -4,12 +4,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     @RequestMapping("/sayHello")
-    public String sayHello(@RequestParam(value = "username") String username){
-        return "Hello!" +username;
+    public String sayHello(@RequestParam(value = "username") String username,
+                           HttpServletRequest request){
+        return "hello,"+username;
+    }
+
+    @RequestMapping("/testHello")
+    public String testHello(@RequestParam(value = "username") String username,
+                           HttpServletRequest request){
+        return "testHello,springboot"+username;
     }
 }
